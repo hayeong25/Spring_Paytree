e794a723d26904c2d54f4e1035f71602ec318ca5
hayeong25 <hayeong0624@gmail.com>
2024-06-17 11:13:55 +0900

    init

---
+++ src/main/java/com/api/paytree/PaytreeApplication.java
@@ -0,0 +1,13 @@
+package com.api.paytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/paytree/PaytreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.paytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
