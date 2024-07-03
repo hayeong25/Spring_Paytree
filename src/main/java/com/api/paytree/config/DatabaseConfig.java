d0aef27129a56b8a0fc64a7e254d0253e996f315
hayeong25 <hayeong0624@gmail.com>
2024-07-03 12:47:47 +0900

    fix: SqlSessionTemplate 제거

--- src/main/java/com/api/paytree/config/DatabaseConfig.java
+++ src/main/java/com/api/paytree/config/DatabaseConfig.java
@@ -2,7 +2,6 @@ package com.api.paytree.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +12,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @MapperScan(basePackages = {"com.api.paytree.mapper"}
-        , sqlSessionFactoryRef = "sqlSessionFactory"
-        , sqlSessionTemplateRef = "sqlSessionTemplate")
+        , sqlSessionFactoryRef = "sqlSessionFactory")
 public class DatabaseConfig {
     @Bean(name = "sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
@@ -26,10 +24,5 @@ public class DatabaseConfig {
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResource("classpath:/mapper/*Mapper.xml"));
 
         return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean(name = "sqlSessionTemplate")
-    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
